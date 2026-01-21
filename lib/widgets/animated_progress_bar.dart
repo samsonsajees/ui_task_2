a6027a06e09876abc81982b6180848c16a45989d
samsonsajees <samsonsajees@gmail.com>
2026-01-22 00:37:03 +0530

    /

--- lib/widgets/animated_progress_bar.dart
+++ lib/widgets/animated_progress_bar.dart
@@ -38,3 +38,36 @@ class CustomProgressBar extends StatelessWidget {
     );
   }
 }
+
+
+
+/*
+import 'package:flutter/material.dart';
+import 'package:ui_task_2/constants/app_colors.dart';
+
+class AnimatedProgressBar extends StatelessWidget {
+  final double progress;
+
+  const AnimatedProgressBar({super.key, required this.progress});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(4),
+        child: TweenAnimationBuilder<double>(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          tween: Tween<double>(begin: 0, end: progress),
+          builder: (context, value, _) => LinearProgressIndicator(
+            value: value,
+            minHeight: 8,
+            backgroundColor: const Color(0xFFF2F2F5),
+            color: AppColors.primaryBlue,
+          ),
+        ),
+      ),
+    );
+  }
+}*/
