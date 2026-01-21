3a410c239f94fafe30178a489daad26a69436f6c
samsonsajees <samsonsajees@gmail.com>
2026-01-22 00:24:41 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: 'Inter',
         scaffoldBackgroundColor: AppColors.white,
         primaryColor: AppColors.primaryBlue,
         useMaterial3: true     
