c50a47f1f2d5da91ade946a32daaf1c2f367e4ee
samsonsajees <samsonsajees@gmail.com>
2026-01-27 23:15:40 +0530

    added new var in mess model

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -4,10 +4,12 @@ class MessageModel {
   final String message;  // Stores "Text" OR "File Path"
   final bool isMyMessage;
   final MessageType type;
+  final bool read;
 
   MessageModel({
     required this.message,
     required this.isMyMessage,
     this.type = MessageType.text,
+    this.read = false,
   });
 }
